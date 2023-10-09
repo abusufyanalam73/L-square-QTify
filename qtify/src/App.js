a4b65e6d151a16156950dfd0eece73190be77614
abusufyanalam73 <abusufyanalam00786@gmail.com>
2023-10-09 23:33:28 +0530

    hero-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-// import Hero from "./components/Hero/Hero";
+import Hero from "./components/Hero/Hero";
 // import Card from "./components/Card/Card";
 
 function App() {
   return (
     <div>
       <Navbar />
-      {/*<Hero />
-      <Card /> */}
+      <Hero />
+      {/* <Card /> */}
     </div>
   );
 }
